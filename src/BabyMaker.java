a6b70a2541c2eec7cc9df1dbdf00016e8e53541b
jonathan.egan <jonathan.egan@MU-C1-12.mullen.pvt.k12.co.us>
2019-10-03 12:40:52 -0600

    commit

--- src/BabyMaker.java
+++ src/BabyMaker.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+
 public class BabyMaker 
 {
 	public static void main(String[] args) 
 	{
-		Nursery.babyNursery();
+		ArrayList<Baby> nursery = new ArrayList<Baby>();
+		nursery.add(new Baby("male", 6.2, "Fred", "Mullen", "football", "dog", 13, "Emma", "rap", "pizza"));
+		nursery.add(new Baby("female", 5.1, "Emma", "Regis", "basketball", "cat", 26, "Jon", "rock", "hot dog"));
+		nursery.add(new Baby("male", 7.3, "Bob", "Lakewood", "baseball", "bird", 7, "Fred", "country", "hamburger"));
+		PrintNursery.printNursery(nursery);
 	}
 }
---
+++ src/PrintNursery.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class PrintNursery 
+{
+	public static void printNursery(ArrayList<Baby> nursery)
+	{
+		for (Baby baby: nursery) 
+		{
+		    System.out.println(baby);
+		}
+	}
+}
