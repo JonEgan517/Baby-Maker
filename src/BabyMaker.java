0e2151b7efb6648fd5d0b5450b16402c304348b5
jonathan.egan <jonathan.egan@MU-C1-12.mullen.pvt.k12.co.us>
2019-10-03 11:55:27 -0600

    commit

--- src/BabyMaker.java
+++ src/BabyMaker.java
@@ -1,19 +1,7 @@
-import java.util.ArrayList;
-
 public class BabyMaker 
 {
 	public static void main(String[] args) 
 	{
-		ArrayList<Baby> nursery = new ArrayList<Baby>();
-		nursery.add(new Baby("male", 6.2, "Fred", "Mullen", "football", "dog", 13, "Emma", "rap", "pizza"));
-		nursery.add(new Baby("female", 5.1, "Emma", "Regis", "basketball", "cat", 26, "Jon", "rock", "hot dog"));
-		nursery.add(new Baby("male", 7.3, "Bob", "Lakewood", "baseball", "bird", 7, "Fred", "country", "hamburger"));
-		
-		alphabetically(nursery);
-	}
-
-	private static void alphabetically(ArrayList<Baby> nursery) 
-	{
-		
+		Nursery.babyNursery();
 	}
 }
